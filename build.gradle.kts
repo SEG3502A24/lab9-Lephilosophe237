110a942b68cf0bf46b753d6c1aa032d5732db862
Joel Stéphane <90732192+Lephilosophe237@users.noreply.github.com>
2024-11-15 12:01:32 -0500

    Laboratoire 9 done. Add new webconverter controller to be able to keep the api controller and replicate the solution of the professor with the loggin https://github.com/stephanesome/tempconverter-security/tree/main

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,13 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
